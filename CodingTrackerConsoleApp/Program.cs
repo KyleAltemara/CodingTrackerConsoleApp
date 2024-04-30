91822358da3c91c74719e2a0c0233b2dc9be91ac
Airiel <Kyle.Altemara@gmail.com>
2024-04-29 17:54:11 -0700

    Solution created

---
+++ CodingTrackerConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+using Dapper;
+using Microsoft.Data.Sqlite;
+
+namespace CodingTrackerConsoleApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
